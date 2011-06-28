0f6e561a8e07168996c9a4c7c8c5f47de702505c
nickflux <nick@bolsovernetworks.com>
2011-06-28 19:36:05 +0100

    made redact form a text area

--- app/models/redact.rb
+++ app/models/redact.rb
@@ -15,7 +15,10 @@ class Redact
   end
 
   def redact_array=(redact_array_val)
-    self[:redact_array] = redact_array_val.delete_if{|r| r == ''}
+    redact_array_temp  = redact_array_val.split("\r\n")
+    Rails.logger.debug redact_array_temp.inspect
+    #Rails.logger.debug redact_array_temp.delete_if{|r| r == ''|| r == '\n' || r == '\r'}
+    self[:redact_array] = redact_array_temp.delete_if{|r| r == ''}
   end
 
 end
