392fb47191fff32ce13626067aea6e944a1e3075
nickflux <nick@bolsovernetworks.com>
2012-03-12 18:38:20 +0000

    update bootup stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-DoitidiotApi::Application.configure do
+Doitidiot::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
